9f8ca5a30562ff83ef57e7735982cd7113087b8d
lincis <linardssils1@gmail.com>
2025-02-05 20:24:04 +0200

    feat: Add ProductCard component for displaying product information on the store page

---
+++ src/components/custom/product-card.tsx
@@ -0,0 +1,26 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import Image from 'next/image';
+
+export const ProductCard = () => {
+  return (
+    <Card className='w-full max-w-xs p-5 cursor-pointer hover:scale-105 duration-200 ease-linear hover:border hover:border-primary'>
+      <Image
+        priority
+        src='https://i.imgur.com/MOZBqVS.jpg'
+        width='400'
+        height='250'
+        alt='Product'
+        className='aspect-[1.6] object-cover object-center'
+      />
+      <CardHeader className='p-4'>
+        <CardTitle className='text-xl truncate'>
+          Product Namesssssssssssss
+        </CardTitle>
+      </CardHeader>
+      <CardContent className='p-4'>
+        <p className='text-base'>4 of 5</p>
+        <p className='text-lg font-semibold'>$199.00</p>
+      </CardContent>
+    </Card>
+  );
+};
