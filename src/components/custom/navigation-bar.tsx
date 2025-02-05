18554bee15ca4c35e36b541b8367af0df561309e
lincis <linardssils1@gmail.com>
2025-02-05 18:28:38 +0200

    chore: Update npm dependencies and add Radix UI navigation menu component

--- src/components/custom/navigation-bar.tsx
+++ src/components/custom/navigation-bar.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link';
 import { Button } from '../ui/button';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu';
 
 // route interface for the links so they are type safe
 interface RouteProps {
   label: string;
   href: string;
+  description?: string;
 }
+
 //  navigation component for the website
 export const NavigationBar = () => {
   return (
@@ -16,29 +26,48 @@ export const NavigationBar = () => {
           <span className='sr-only'>Shop Sphere</span>
         </Link>
         <nav className='hidden md:flex gap-4'>
-          {links.map(({ label, href }) => (
-            <Link
-              key={href}
-              href={href}
-              className='relative font-semibold flex items-center text-sm transition-colors 
-                    after:absolute after:h-[3px] after:bg-foreground after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center after:bottom-0 after:left-0'
-              prefetch={true}
-            >
-              {label}
-            </Link>
-          ))}
+          {links.map(({ label, href }) =>
+            label === 'Veikals' ? (
+              <NavigationMenu key={label}>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className='relative font-semibold flex items-center text-sm transition-colors '>
+                      Veikals
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] '>
+                        {storeLinks.map((link) => (
+                          <ListItem key={link.label} {...link}>
+                            {link.description}
+                          </ListItem>
+                        ))}
+                      </ul>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            ) : (
+              <Link
+                key={href}
+                href={href}
+                className='relative font-semibold flex items-center text-sm transition-colors 
+                  after:absolute after:h-[3px] after:bg-foreground after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center after:bottom-0 after:left-0'
+                prefetch={true}
+              >
+                {label}
+              </Link>
+            ),
+          )}
         </nav>
         <div className='flex items-center gap-4'>
-          <>
-            <Link href='#' className='hidden md:flex'>
-              <Button variant='outline' size='sm'>
-                Pierakstīties
-              </Button>
-            </Link>
-            <Link href='#' className='hidden md:flex'>
-              <Button size='sm'>Reģistrēties</Button>
-            </Link>
-          </>
+          <Link href='#' className='hidden md:flex'>
+            <Button variant='outline' size='sm'>
+              Pierakstīties
+            </Button>
+          </Link>
+          <Link href='#' className='hidden md:flex'>
+            <Button size='sm'>Reģistrēties</Button>
+          </Link>
         </div>
       </div>
     </header>
@@ -47,7 +76,7 @@ export const NavigationBar = () => {
 
 const links: RouteProps[] = [
   {
-    label: '1',
+    label: 'Veikals',
     href: '/',
   },
   {
@@ -55,7 +84,57 @@ const links: RouteProps[] = [
     href: '/1',
   },
   {
-    label: '3',
-    href: '/2',
+    label: 'Par mums',
+    href: '/about',
   },
 ];
+
+const storeLinks: RouteProps[] = [
+  {
+    label: 'Visas preces',
+    href: '/products',
+    description:
+      'Izvēlies no tūkstošiem produktiem, kas pieejami jums vienā vietā.',
+  },
+  {
+    label: 'Izpārdošana',
+    href: '/sale',
+    description:
+      'Izvēlies no tūkstošiem produktu, kuri ir pieejami par zemākām cenām.',
+  },
+  {
+    label: 'Jaunumi',
+    href: '/new',
+    description: 'Jauni produkti, kas nesen pievienoti.',
+  },
+  {
+    label: 'Kategorijas',
+    href: '/categories',
+    description:
+      'Izvēlies produktus pēc kategorijām, lai vieglāk atrastu to, ko meklē.',
+  },
+];
+
+const ListItem = ({
+  label,
+  href,
+  children,
+}: RouteProps & { children?: string }) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <Link
+          className={
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground'
+          }
+          href={href}
+        >
+          <div className='text-sm font-medium leading-none'>{label}</div>
+          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+            <>{children}</>
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+};
