1ed3a5e5e675c226189be069d34be6b7232b5940
lincis <linardssils1@gmail.com>
2025-02-05 20:24:17 +0200

    feat: Add lazy loading for images and update product page layout

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,4 @@
+import { ProductCard } from '@/components/custom/product-card';
 import { SideFilters } from '@/components/custom/side-filters';
 
 export default function Products() {
@@ -7,7 +8,17 @@ export default function Products() {
         <section className='mt-3'>
           <h1 className='font-semibold text-3xl'>Visi produkti</h1>
         </section>
-        <SideFilters />
+        <div className='flex flex-row gap-8'>
+          <SideFilters />
+          <section className='flex-1 grid  gap-x-2 gap-y-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-cols-1'>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </section>
+        </div>
       </div>
     </div>
   );
