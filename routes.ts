a919b601c2bd4e356e96accb85ba94ebbff48a5b
lincis <linardssils1@gmail.com>
2025-02-05 21:04:09 +0200

    feat: Update public and auth routes

---
+++ routes.ts
@@ -0,0 +1,16 @@
+export const publicRoutes = [
+  '/',
+  '/about',
+  '/products',
+  '/products/[id]',
+  '/sale',
+  '/new',
+  '/categories',
+];
+export const authRoutes = ['/auth/sign-in', '/auth/sign-up'];
+
+export const DEFAULT_SIGNOUT_REDIRECT = '/products';
+
+export const DEFAULT_SIGNIN_REDIRECT = '/products';
+
+export const DEFAULT_SIGNUP_REDIRECT = '/products';
