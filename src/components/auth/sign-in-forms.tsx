13d39bc9cae54fbf76e20e8fd97532a7de325c02
lincis <linardssils1@gmail.com>
2025-02-06 22:43:21 +0200

    feat: Add sign-in and sign-up forms with validation and submission logic

--- src/components/auth/sign-in-forms.tsx
+++ src/components/auth/sign-in-forms.tsx
@@ -1,12 +1,96 @@
+'use client';
 import { CardWrapper } from './card-wrapper';
+import { z } from 'zod';
+import { signInSchema } from '../../../schemas';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { startTransition } from 'react';
+import { login } from '../../../actions/login';
 
 export const SignInForms = () => {
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof signInSchema>) => {
+    console.log(data);
+
+    startTransition(() => {
+      login(data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    });
+  };
+
   return (
     <CardWrapper
       formType='Pieslēgšanās'
       label='Pieslēdzies, lai izmantotu visas funkcijas'
     >
-      <div>Hello sign in forms</div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex flex-col space-y-8'
+        >
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Epasts</FormLabel>
+                <FormControl>
+                  <Input
+                    type='email'
+                    placeholder='john.doe@example.com'
+                    {...field}
+                    required
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Parole</FormLabel>
+                <FormControl>
+                  <Input
+                    type='password'
+                    placeholder='********'
+                    {...field}
+                    required
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit' className='mt-8'>
+            Pieslēgties
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
--- src/components/auth/sign-up-forms.tsx
+++ src/components/auth/sign-up-forms.tsx
@@ -1,12 +1,113 @@
+'use client';
+import { useForm } from 'react-hook-form';
 import { CardWrapper } from './card-wrapper';
+import { z } from 'zod';
+import { signUpSchema } from '../../../schemas';
+import { startTransition } from 'react';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { register } from '../../../actions/register';
 
 export const SignUpForms = () => {
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    console.log(data);
+
+    startTransition(() => {
+      register(data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    });
+  };
+
   return (
     <CardWrapper
       formType='Reģistrācija'
       label='Reģistrējies, lai izmantotu visas funkcijas'
     >
-      <div>Hello sign in forms</div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex flex-col space-y-8'
+        >
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Epasts</FormLabel>
+                <FormControl>
+                  <Input
+                    type='email'
+                    placeholder='john.doe@example.com'
+                    {...field}
+                    required
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Parole</FormLabel>
+                <FormControl>
+                  <Input
+                    type='password'
+                    placeholder='********'
+                    {...field}
+                    required
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='passwordConfirmation'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Parole vcelvienreiz</FormLabel>
+                <FormControl>
+                  <Input
+                    type='password'
+                    placeholder='********'
+                    {...field}
+                    required
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit' className='mt-8'>
+            Registreties
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
