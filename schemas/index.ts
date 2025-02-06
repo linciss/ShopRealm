7e4d2677ad2b6dc0620430a5cd56c2e9bcd5f20f
lincis <linardssils1@gmail.com>
2025-02-06 22:43:36 +0200

    feat: Add sign-in and sign-up form validation schemas

---
+++ schemas/index.ts
@@ -0,0 +1,51 @@
+import { z } from 'zod';
+
+export const signInSchema = z
+  .object({
+    email: z
+      .string()
+      .email({ message: 'Lūdzu ievadiet pareizu epastu' })
+      .min(1, { message: 'Lūdzu ievadiet epastu' }),
+    password: z
+      .string()
+      .min(8, { message: 'Parolei jābūt vismaz 8 simbolus garai' }),
+  })
+  .superRefine(({ password }, validate) => {
+    // checks for the appropriate password strength
+    const containsLowerCase = /[a-z]/.test(password);
+    const containsUpperCase = /[A-Z]/.test(password);
+    const containsSpecialCharacter =
+      /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password);
+    const containsNumber = /[0-9]/.test(password);
+
+    if (
+      !containsLowerCase ||
+      !containsUpperCase ||
+      !containsSpecialCharacter ||
+      !containsNumber
+    ) {
+      validate.addIssue({
+        code: 'custom',
+        path: ['password'],
+        message:
+          'Parole ir par vāju. Parolei jāsatur vismaz viens lielais burts, viens mazais burts, viens cipars un viens speciālais simbols.',
+      });
+    }
+  });
+
+export const signUpSchema = z
+  .object({
+    email: z.string().email({ message: 'Lūdzu ievadiet pareizu epastu' }),
+    password: z
+      .string()
+      .min(8, { message: 'Parolei jābūt vismaz 8 simbolus garai' }),
+    passwordConfirmation: z.string(),
+  })
+  .refine(({ password, passwordConfirmation }) => {
+    if (password !== passwordConfirmation) {
+      return {
+        passwordConfirmation: 'Paroles nesakrīt',
+      };
+    }
+    return true;
+  });
