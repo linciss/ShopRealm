d401052511ad891a277659d0a515b17b4d383341
lincis <linardssils1@gmail.com>
2025-02-09 19:55:02 +0200

    feat: adding some middleware logic so the app kinda start to make sense

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,36 @@
 import authConfig from '../auth.config';
 import NextAuth from 'next-auth';
+import { apiAuthPrefix, authRoutes, publicRoutes } from '../routes';
+import { NextResponse } from 'next/server';
+import { DEFAULT_SIGNIN_REDIRECT } from './../routes';
 
 const { auth } = NextAuth(authConfig);
 
 export default auth(async (req) => {
+  const { nextUrl } = req;
+
   const isLoggedIn = !!req.auth;
 
-  console.log(isLoggedIn, 'is logged in');
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  console.log(req.auth, 'sss');
+  if (isApiAuthRoute) {
+    return NextResponse.next();
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL(DEFAULT_SIGNIN_REDIRECT, nextUrl));
+    }
+    return NextResponse.next();
+  }
+
+  if (!isPublicRoute && !isLoggedIn) {
+    return NextResponse.redirect(new URL('/auth/sign-in', nextUrl));
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
