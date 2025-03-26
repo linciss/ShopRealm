6aba6b6631f48768309f8dcc6cc3e4dc692d7b8c
lincis <linardssils1@gmail.com>
2025-03-26 14:55:03 +0200

    feat: Implement fallback redirect for unauthorized access in ProtectedLayout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,4 +1,6 @@
 import { auth } from '../../../auth';
+import { redirect } from 'next/navigation';
+import { FALLBACK_REDIRECT } from '../../../routes';
 
 export default async function ProtectedLayout({
   children,
@@ -7,9 +9,11 @@ export default async function ProtectedLayout({
 }) {
   const session = await auth();
 
+  // if user accesses private routes wihtout session they are reidrect to fallback url which logs them out
   if (!session?.user?.id) {
-    return;
+    redirect(FALLBACK_REDIRECT);
   }
+
   return (
     <div className='flex min-h-screen flex-col'>
       <div className='flex flex-1'>
