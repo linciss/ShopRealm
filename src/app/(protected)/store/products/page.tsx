0221590a909f27d49da33d1c0809c4345284b24e
lincis <linardssils1@gmail.com>
2025-03-28 17:33:00 +0200

    feat: Update link for adding products to point to configurator page

--- src/app/(protected)/store/products/page.tsx
+++ src/app/(protected)/store/products/page.tsx
@@ -12,7 +12,7 @@ export default async function Products() {
       <div className='flex justify-between items-center flex-row gap-4'>
         <h1 className=' font-bold text-3xl '>Produkti</h1>
         <div className='space-x-2 flex-nowrap flex flex-row'>
-          <Link href={`/store/products/new`} prefetch={true}>
+          <Link href={`/store/products/configurator`} prefetch={true}>
             <Button>
               <Plus />
               Pievienot preci
