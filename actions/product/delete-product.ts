fb5dd3285a661a7d8bd05328380fcebfe0f58c9d
lincis <linardssils1@gmail.com>
2025-03-30 20:30:11 +0300

    feat: Enhance delete product functionality with improved error handling and cache revalidation

--- actions/product/delete-product.ts
+++ actions/product/delete-product.ts
@@ -3,6 +3,8 @@
 import prisma from '@/lib/db';
 import { auth } from '../../auth';
 import { getStoreId } from '../../data/store';
+import { revalidatePath } from 'next/cache';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 export const deleteProduct = async (productId: string) => {
   const session = await auth();
@@ -14,25 +16,19 @@ export const deleteProduct = async (productId: string) => {
 
     if (!storeId) return { error: 'Kluda' };
 
-    const product = await prisma.product.findUnique({
-      where: { id: productId },
-      select: { storeId: true },
-    });
-
-    if (!product) return { error: 'Kluda! nav atrast produkts' };
-
-    if (storeId !== product.storeId)
-      return { error: 'Nav jusu veiakal produkts' };
-
     await prisma.product.delete({
-      where: { id: productId },
+      where: { id: productId, storeId: storeId },
     });
 
+    revalidatePath('/store/products');
     return { success: 'izdzest produkts!~' };
   } catch (error) {
-    if (error instanceof Error) {
-      console.log('Error: ', error.stack);
+    // return prisma error so i dont have to query database to check if product exists
+    if (error instanceof PrismaClientKnownRequestError) {
+      if (error.code === 'P2025') return { error: 'Nav atrasts produkts!' };
+      return { error: 'Kļūda apstrādājot datus' };
+    } else {
+      return { error: 'Kļūda apstrādājot datus' };
     }
-    return { error: 'Kļūda apstrādājot datus' };
   }
 };
