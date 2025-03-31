ede0a113fb4c87a9b50225c09ffc1d8b7a4e281e
lincis <linardssils1@gmail.com>
2025-03-31 20:03:30 +0300

    feat: Add getLocalCartProducts function to retrieve products from local cart by IDs

---
+++ actions/cart/get-local-cart-items.ts
@@ -0,0 +1,25 @@
+'use server';
+import prisma from '@/lib/db';
+
+export const getLocalCartProducts = async (productIds: string[]) => {
+  try {
+    if (!productIds.length) return { error: 'Kluda!' };
+
+    const products = await prisma.product.findMany({
+      where: { id: { in: productIds } },
+      select: {
+        name: true,
+        id: true,
+        price: true,
+        image: true,
+      },
+    });
+
+    return { products };
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err);
+    }
+    return { error: 'KLUDA!' };
+  }
+};
