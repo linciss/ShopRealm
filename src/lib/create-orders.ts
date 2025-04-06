51e7ea81df9a9bed0dda2d281551a78410915122
lincis <linardssils1@gmail.com>
2025-04-06 23:15:14 +0300

    feat: Enhance order creation logic to include payment intent ID and improve error handling; update cart item processing

--- src/lib/create-orders.ts
+++ src/lib/create-orders.ts
@@ -5,58 +5,77 @@ export const createOrdersFromSession = async (session: any) => {
   const cartId = session.metadata.cartId;
   const userId = session.client_reference_id;
 
-  //   creeates the respective orde r
-  const order = await prisma.order.create({
-    data: {
-      userId: userId as string,
-      stripeSessionId: session.id,
-      paymentStatus: 'paid',
-      amount: session.amount_total / 100,
-    },
-  });
+  try {
+    //   creeates the respective orde r
+    const order = await prisma.order.create({
+      data: {
+        userId: userId as string,
+        stripeSessionId: session.id,
+        paymentStatus: 'paid',
+        paymentIntentId: session.payment_intent,
+        amount: session.amount_total / 100,
+      },
+    });
 
-  //   finds user cart items
-  const cartItems = await prisma.cartItem.findMany({
-    where: { cartId },
-    include: { product: true },
-  });
+    //   finds user cart
+    const cart = await prisma.cart.findUnique({
+      where: { id: session.metadata.cartId },
+      include: { cartItems: { include: { product: true } } },
+    });
 
-  //   makes orderitems for order
-  await Promise.all(
-    cartItems.map((item) =>
-      prisma.orderItem.create({
-        data: {
-          orderId: order.id,
-          productId: item.productId,
-          storeId: item.product.storeId,
-          quantity: item.quantity,
-          priceAtOrder: parseFloat(item.product.price),
-          total: item.quantity * parseFloat(item.product.price),
-        },
+    if (!cart) return { error: 'Cart not found' };
+
+    // calcualtes the payment date
+    const transferScheduleDate = new Date();
+    // for testing purposes
+    transferScheduleDate.setDate(transferScheduleDate.getDate());
+    // transferScheduleDate.setDate(transferScheduleDate.getDate() + 14);
+
+    // creates an orderitem
+    await Promise.all(
+      cart.cartItems.map(async (item) => {
+        await prisma.orderItem.create({
+          data: {
+            orderId: order.id,
+            productId: item.productId,
+            storeId: item.product.storeId,
+            quantity: item.quantity,
+            priceAtOrder: parseFloat(item.product.price),
+            total: parseFloat(item.product.price) * item.quantity,
+            status: 'pending',
+            escrowStatus: 'holding',
+            transferScheduledFor: transferScheduleDate,
+          },
+        });
       }),
-    ),
-  );
+    );
 
-  //   updates the quantity for product
-  await Promise.all(
-    cartItems.map((item) => {
-      const newQuantity = Math.max(0, item.product.quantity - item.quantity);
-      const isOutOfStock = newQuantity === 0;
+    // updates quantity of product
+    await Promise.all(
+      cart.cartItems.map((item) => {
+        const newQuantity = Math.max(0, item.product.quantity - item.quantity);
+        const isOutOfStock = newQuantity === 0;
 
-      return prisma.product.update({
-        where: { id: item.productId },
-        data: {
-          quantity: newQuantity,
-          ...(isOutOfStock ? { isActive: false } : {}),
-        },
-      });
-    }),
-  );
+        return prisma.product.update({
+          where: { id: item.productId },
+          data: {
+            quantity: newQuantity,
+            ...(isOutOfStock ? { isActive: false } : {}),
+          },
+        });
+      }),
+    );
 
-  //   delte cart items
-  await prisma.cartItem.deleteMany({
-    where: { cartId },
-  });
+    //   delte cart items
+    await prisma.cartItem.deleteMany({
+      where: { cartId },
+    });
 
-  return order;
+    return order;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err);
+    }
+    return { error: 'Kluda!' };
+  }
 };
