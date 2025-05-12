659a43a848340d1b2cd9144846e62fd118add2d6
lincis <linardssils1@gmail.com>
2025-05-12 19:14:31 +0300

    feat: Enhance locale change button logic to handle additional URL splits

--- src/components/custom/profile/settings/change-locale-button.tsx
+++ src/components/custom/profile/settings/change-locale-button.tsx
@@ -30,6 +30,12 @@ export const ChangeLocaleButton = ({
       router.push(`/${localeChange}/${split[1]}`);
     } else if (pathname.slice(1) === params.locale) {
       router.push(`/${localeChange}/${split[0]}`);
+      // when 3 splits /locale/a/b
+    } else if (split.length > 3 && params.locale !== 'en') {
+      router.push(`/${localeChange}/${split[2]}/${split[3]}`);
+      // when 3 splits but since en isnt showing up so have to handle it /locale/a/b
+    } else if (split.length === 3 && params.locale === 'en') {
+      router.push(`/${localeChange}/${split[1]}/${split[2]}`);
     } else {
       router.push(`/${localeChange}/${split[2]}`);
     }
